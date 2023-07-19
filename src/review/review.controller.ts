f2d037aad4b5b3bcc851ebf195be22f4c0fee971
RuslanSalimov <mail-spam2012@yandex.ru>
2023-07-19 23:42:22 +0500

    create controllers

---
+++ src/review/review.controller.ts
@@ -0,0 +1,19 @@
+import {Body, Controller, Delete, Get, Param, Post} from '@nestjs/common';
+import {ReviewModel} from "./review.model/review.model";
+
+@Controller('review')
+export class ReviewController {
+    @Post('create')
+    async create(@Body() dto: Omit<ReviewModel, '_id'>) {
+
+    }
+
+    @Delete
+    async delete(@Param('id') id: string) {
+
+    }
+    @Get('byProduct/:productId')
+    async getByProduct(@Param('id') id: string){
+
+    }
+}
